c4b6f3d9ee9778484346663838d72f8df9389658
José Manuel Rodríguez Camacho <josemanuel0709@hotmail.es>
2024-08-29 22:28:01 +0200

    Avances maquetación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,10 @@ import { ThemeService } from './services/theme.service';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    standalone: true,
-    imports: [RouterOutlet],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [RouterOutlet],
 })
 export class AppComponent {
   title = 'Jose Manuel Rodríguez Camacho - Web Developer';
