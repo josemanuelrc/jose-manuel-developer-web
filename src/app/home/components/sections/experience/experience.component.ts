f2aca05671b30760e159d2eecd57bedb348a3757
José Manuel Rodríguez Camacho <josemanuel0709@hotmail.es>
2024-09-28 17:05:01 +0200

    Avances maquetacion: timeline y about-me image

--- src/app/home/components/sections/experience/experience.component.ts
+++ src/app/home/components/sections/experience/experience.component.ts
@@ -6,4 +6,109 @@ import { Component } from '@angular/core';
   styleUrls: ['./experience.component.scss'],
   standalone: true,
 })
-export class ExperienceComponent {}
+export class ExperienceComponent {
+  experiences: Experience[] = [
+    {
+      title: 'Desarrollador Backend',
+      company: 'TEMPE Inditex',
+      period: '2024 - 2024',
+      description: 'Diseño y Desarrollo de Portal Web para Datalake de IFAPA',
+      technologies: [
+        'Java',
+        'Spring',
+        'SpringBoot',
+        'Hibernate',
+        'MySQL',
+        'Git',
+        'GitHub',
+      ],
+    },
+    {
+      title: 'Desarrollador Web Full-Stack',
+      company: 'SOLTEL Group',
+      period: '2023 - 2024',
+      description: 'Diseño y Desarrollo de Portal Web para Datalake de IFAPA',
+      technologies: [
+        'HTML',
+        'SCSS',
+        'JavaScript',
+        'TypeScript',
+        'Node.js',
+        'Angular',
+        'RxJs',
+        'NgRx',
+        'Java',
+        'Spring',
+        'SpringBoot',
+        'Microservicios',
+        'Hibernate',
+        'SQL',
+        'MySQL',
+        'Git',
+        'GitLab',
+      ],
+    },
+    {
+      title: 'Desarrollador Web Full-Stack',
+      company: 'Ayesa Advanced Technologies',
+      period: '2022 - 2023',
+      description: 'Diseño y Desarrollo de Portal Web para Datalake de IFAPA',
+      technologies: [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'TypeScript',
+        'Node.js',
+        'Angular',
+        'RxJs',
+        'Bootstrap',
+        'Java',
+        'Spring',
+        'SpringBoot',
+        'Microservicios',
+        'Hibernate',
+        'SQL',
+        'PL/SQL',
+        'Oracle',
+        'Git',
+        'GitLab',
+      ],
+    },
+    {
+      title: 'Programador Junior',
+      company: 'SGRSoft',
+      period: '2022 - 2022',
+      description: 'Diseño y Desarrollo de Portal Web para Datalake de IFAPA',
+      technologies: ['C#', '.NET Core', '.NET Framework', 'SQL', 'SQLServer'],
+    },
+    {
+      title: 'Desarrollador Web Full-Stack',
+      company: 'Chapham Software Studio',
+      period: '2021 - 2021',
+      description: 'Diseño y Desarrollo de Portal Web para Datalake de IFAPA',
+      technologies: [
+        'HTML',
+        'SASS',
+        'JavaScript',
+        'TypeScript',
+        'Node.js',
+        'Angular',
+        'RxJs',
+        'PrimeNG',
+        'Java',
+        'Spring',
+        'SpringBoot',
+        'Hibernate',
+        'PostgreSQL',
+      ],
+    },
+  ];
+}
+
+interface Experience {
+  title: string;
+  company: string;
+  period: string;
+  description: string;
+  technologies?: string[];
+}
