a9fb6e73d5bf3000406f7403bafa5c685c9d6839
José Manuel Rodríguez Camacho <josemanuel0709@hotmail.es>
2024-09-29 02:19:27 +0200

    Pequeñas correcciones y adiciones + descripciones experiencias.

--- src/app/home/components/sections/education/education.component.ts
+++ src/app/home/components/sections/education/education.component.ts
@@ -7,19 +7,63 @@ import { Component } from '@angular/core';
   standalone: true,
 })
 export class EducationComponent {
-  education = [
+  titles: TitleItem[] = [
     {
       title: 'Grado en Ingeniería Informática',
-      date: '2016 - 2021 | Universidad de Huelva',
+      period: '2016 - 2021',
+      institution: 'Universidad de Huelva',
       description: 'T.F.G.: Arquitectura de Computador basada en Acumulador',
     },
     {
       title: 'Bachillerato Bilingüe, Ciencias y Tecnologías',
-      date: '2014 - 2016 | I.E.S. Catedrático Pulido Rubio',
+      period: '2014 - 2016',
+      institution: 'I.E.S. Catedrático Pulido Rubio',
       description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
                     Illum eius blanditiis dolorum reprehenderit asperiores 
                     pariatur, non libero aspernatur omnis. Dolores quod cum 
                     delectus eligendi qui nobis commodi nesciunt voluptas sit.`,
     },
   ];
+
+  courses: CourseItem[] = [
+    {
+      title: 'Curso de Angular',
+      period: '2022',
+      institution: 'Academia XYZ',
+      description: 'Descripción del curso.',
+    },
+    {
+      title: 'Curso de React',
+      period: '2023',
+      institution: 'Academia ABC',
+      description: 'Descripción del curso.',
+    },
+  ];
+
+  languages = [
+    {
+      name: 'Inglés',
+      level: 'C1',
+      description: 'Competente en comunicación verbal y escrita.',
+    },
+    {
+      name: 'Francés',
+      level: 'B2',
+      description: 'Buena comprensión y expresión escrita.',
+    },
+  ];
+}
+
+interface TitleItem {
+  title: string;
+  period: string;
+  institution: string;
+  description: string;
+}
+
+interface CourseItem {
+  title: string;
+  period: string;
+  institution: string;
+  description: string;
 }
--- src/app/home/components/sections/experience/experience.component.ts
+++ src/app/home/components/sections/experience/experience.component.ts
@@ -12,12 +12,16 @@ export class ExperienceComponent {
       title: 'Desarrollador Backend',
       company: 'TEMPE Inditex',
       period: '2024 - 2024',
-      description: 'Diseño y Desarrollo de Portal Web para Datalake de IFAPA',
+      description: [
+        '• Diseño y Desarrollo de Sistema de Gestión de referencias e inventario.',
+      ],
       technologies: [
         'Java',
         'Spring',
         'SpringBoot',
-        'Hibernate',
+        'Microservicios',
+        'Swagger',
+        'JPA/Hibernate',
         'MySQL',
         'Git',
         'GitHub',
@@ -27,7 +31,21 @@ export class ExperienceComponent {
       title: 'Desarrollador Web Full-Stack',
       company: 'SOLTEL Group',
       period: '2023 - 2024',
-      description: 'Diseño y Desarrollo de Portal Web para Datalake de IFAPA',
+      description: [
+        '• Diseño y Desarrollo de Portal Web para Datalake de IFAPA - Catálogo de Datos Científicos Abierto.',
+        '• Entre mis funciones residen la toma de los distintos requerimientos, el diseño y análisis de la \
+        mejor solución a aplicar y la posterior implementación de las funcionalidades requeridas en las \
+        diversas pantallas del portal.',
+        '• Ejerzo de responsable FrontEnd, velando por que se sigan las mejores prácticas posibles, tal como \
+        Clean Code, SOLID, etc... manteniendo en todo momento un código altamente escalable y eficiente.',
+        '• Eventualmente ostento el rol de Scrum Master, coordinando algunas reuniones: dailys, retrospectivas \
+        y plannings de las tareas a realizar en los sprints',
+        '• También suelo colaborar activamente con el equipo de BackEnd para lograr un mayor entendimiento \
+        del dominio del problema y aportar ideas y soluciones efectivas entre ambos.',
+        '• Docencia como profesor en curso de desarrollo web y dispositivos móviles, como parte del plan \
+        INNOVA, impartiendo el módulo de Angular (Componentes, Directivas, Módulos, Observables, etc... ) \
+        y Spring (API REST, Maven, JPA/Hibernate, MySQL, etc...)',
+      ],
       technologies: [
         'HTML',
         'SCSS',
@@ -41,7 +59,8 @@ export class ExperienceComponent {
         'Spring',
         'SpringBoot',
         'Microservicios',
-        'Hibernate',
+        'Swagger',
+        'JPA/Hibernate',
         'SQL',
         'MySQL',
         'Git',
@@ -52,7 +71,19 @@ export class ExperienceComponent {
       title: 'Desarrollador Web Full-Stack',
       company: 'Ayesa Advanced Technologies',
       period: '2022 - 2023',
-      description: 'Diseño y Desarrollo de Portal Web para Datalake de IFAPA',
+      description: [
+        '• Diseño y Desarrollo de Simulador de Ejecución Presupuestaria (SEPRE) para la Junta de Andalucía \
+        como parte del proyecto GOTA.',
+        '• Entre mis responsabilidades se hallaban el desarrollo integral de las funcionalidades requeridas, \
+        incluidas todas las fases del proceso, tanto en el Backend como en el FrontEnd, ejecutando la solución \
+        bajo una arquitectura de micro-servicios que incluía un BFF en conjunto con una basada en Angular para \
+        el FrontEnd.',
+        '• A destacar, entre otras tareas: análisis de requisitos y propuesta de soluciones, modelado y \
+        normalización de la bases de datos, desarrollo y documentación de lógica de negocio necesaria, extracción \
+        de grandes cantidades de datos de archivos estaticos, creación, testeo y consumo de los distintos endpoints de los \
+        microservicios, maquetación y creación de componentes, pantallas, etc...',
+        '• Como parte de mi formación, realice sendos cursos sobre Angular y Spring',
+      ],
       technologies: [
         'HTML',
         'CSS',
@@ -66,7 +97,9 @@ export class ExperienceComponent {
         'Spring',
         'SpringBoot',
         'Microservicios',
-        'Hibernate',
+        'Swagger',
+        'JUnit',
+        'JPA/Hibernate',
         'SQL',
         'PL/SQL',
         'Oracle',
@@ -78,14 +111,19 @@ export class ExperienceComponent {
       title: 'Programador Junior',
       company: 'SGRSoft',
       period: '2022 - 2022',
-      description: 'Diseño y Desarrollo de Portal Web para Datalake de IFAPA',
+      description: [
+        '• Diseño y Desarrollo de aplicación de registro horario en entorno .NET.',
+      ],
       technologies: ['C#', '.NET Core', '.NET Framework', 'SQL', 'SQLServer'],
     },
     {
       title: 'Desarrollador Web Full-Stack',
       company: 'Chapham Software Studio',
       period: '2021 - 2021',
-      description: 'Diseño y Desarrollo de Portal Web para Datalake de IFAPA',
+      description: [
+        '• Desarrollo de aplicación web para gestión de empresas hortofrutícolas.',
+        '• Formación en diversas tecnologias web (Frontend y Backend)',
+      ],
       technologies: [
         'HTML',
         'SASS',
@@ -109,6 +147,6 @@ interface Experience {
   title: string;
   company: string;
   period: string;
-  description: string;
+  description: string[];
   technologies?: string[];
 }
