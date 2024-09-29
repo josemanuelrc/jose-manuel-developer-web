4039d7c973756895b41990ebbfba81110ca0fc03
José Manuel Rodríguez Camacho <josemanuel0709@hotmail.es>
2024-09-29 20:49:01 +0200

    Centered Title, adjustments an menu fixed

--- src/app/home/components/sections/title/title.component.ts
+++ src/app/home/components/sections/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { SvgIconComponent } from '../../svg-icon/svg-icon.component';
 
 @Component({
@@ -8,4 +8,10 @@ import { SvgIconComponent } from '../../svg-icon/svg-icon.component';
   standalone: true,
   imports: [SvgIconComponent],
 })
-export class TitleComponent {}
+export class TitleComponent {
+  @ViewChild('link', { static: false }) link: ElementRef | undefined;
+
+  downloadFile() {
+    this.link?.nativeElement.click();
+  }
+}
